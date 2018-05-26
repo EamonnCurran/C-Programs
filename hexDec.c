0a90d944438ba30595cc50770e418100f17e4503
eamonn <eamonn@ted.home>
2018-05-26 20:19:15 +0100

    Adding choices

--- hexDec.c
+++ hexDec.c
@@ -2,11 +2,20 @@
 #include <stdlib.h>
 
 int main(){
-  int hex;
+  int hex, dec, choice;
 
-  printf("Enter a hex number: ");
-  scanf("%X",&hex);
+  printf("Hex-Dec/Dec-Hex converter.\n    Option 1 is Hex to Dec.\n    Option 2 is Dec to Hex.\n    Please enter 1 or 2: ");
+  scanf("%d",&choice);
 
-  printf("%d\n",hex);
+  if(choice==1){
+    printf("Enter a hexadecimal number: ");
+    scanf("%X",&hex);
+    printf("The decimal value is: %d\n",hex);
+  }
+  else{
+    printf("Enter a decimal number: ");
+    scanf("%d",&dec);
+    printf("The hex value is: %X\n",dec);
+  }
   return 0;
 }
